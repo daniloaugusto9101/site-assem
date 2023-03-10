518af4b8cbc63cafd8f84de0f3540c376f0c7ca2
Danilo Augusto <daniloaugusto9101@gmail.com>
2023-03-10 09:39:09 -0300

    Primeira fase do site concluida

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var swiper = new Swiper(".mySwiper", {
+let destaque__swiper = new Swiper(".destaque__swiper", {
     spaceBetween: 30,
     centeredSlides: true,
     effect: "fade",
@@ -17,33 +17,7 @@ var swiper = new Swiper(".mySwiper", {
   });
 
 
-  const progressCircle = document.querySelector(".autoplay-progress svg");
-  const progressContent = document.querySelector(".autoplay-progress span");
-  var swiper = new Swiper(".mySwiper2", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 500000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev"
-    },
-    on: {
-      autoplayTimeLeft(s, time, progress) {
-        progressCircle.style.setProperty("--progress", 1 - progress);
-        progressContent.textContent = `${Math.ceil(time / 1000)}s`;
-      }
-    }
-  });
-
-
-  var swiper = new Swiper(".mySwiper3", {
+let atividades__swiper = new Swiper(".atividades__swiper", {
     effect: "coverflow",
     grabCursor: true,
     centeredSlides: true,
@@ -59,23 +33,3 @@ var swiper = new Swiper(".mySwiper", {
       el: ".swiper-pagination",
     },
   });
-
-
-  // Menu mobile
-  const btnMobile = document.getElementById('btn-mobile');
-
-  function toggleMenu(event) {
-    if (event.type === 'touchstart') event.preventDefault();
-    const nav = document.getElementById('nav');
-    nav.classList.toggle('active');
-    const active = nav.classList.contains('active');
-    event.currentTarget.setAttribute('aria-expanded', active);
-    if (active) {
-      event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
-    } else {
-      event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
-    }
-  }
-  
-  btnMobile.addEventListener('click', toggleMenu);
-  btnMobile.addEventListener('touchstart', toggleMenu);
