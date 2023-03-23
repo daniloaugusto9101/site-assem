bfd2493bd408ffd6908da126dd924c4e7bb3278a
Danilo Augusto <daniloaugusto9101@gmail.com>
2023-03-23 16:13:47 -0300

    Add page contato

---
+++ page-contato.php
@@ -0,0 +1,93 @@
+<?php
+    // Template Name: Page-contato
+?>
+
+<?php get_header(); ?>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+      <section class="pgContato">
+         
+         <div class="container pgContato__container">
+               <h1 class="tituloHr pgContato__tituloHr">Contatos</h1>
+               <div class="pgContato__item">
+                  <div class="pgContato__mapa">
+
+                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d117363.24103043826!2d-46.01147174835208!3d-23.184748042815002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cc4a24ca59960d%3A0x38aa0bafd01cbb19!2sASSEM%20-%20Associa%C3%A7%C3%A3o%20dos%20Servidores%20Municipais%20de%20S%C3%A3o%20Jos%C3%A9%20dos%20Campos!5e0!3m2!1spt-BR!2sbr!4v1679593010081!5m2!1spt-BR!2sbr" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                     
+                  </div>
+
+                  <div class="pgContato__local">
+                     <h2 class="pgContato__local-title">Sede</h2>
+                     <address>
+                        <div class="pgContato__local-item">
+                           <i class="fa-solid fa-location-dot pgContato__local-ico"></i>
+                           <p class="pgContato__local-endereco"> R. Conselheiro Rodrigues Alves, 190 - Vila Santa Luzia, São José dos Campos - SP, 12209-540</p>
+                        </div>                        
+                        <div class="pgContato__local-item">
+                        <i class="fa-brands fa-square-whatsapp pgContato__local-ico"></i>
+                           <p class="pgContato__local-tel">(12) 3922-7424</p>
+                        </div>                        
+                     </address>     
+                  </div>
+
+               </div>   
+               
+               <div class="pgContato__item">
+                  <div class="pgContato__mapa">
+
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14668.600921047024!2d-45.7671468!3d-23.2011951!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cc4d4d4484e27f%3A0xf9a67400cb912625!2sClube%20de%20Campo%20ASSEM!5e0!3m2!1spt-BR!2sbr!4v1679598262677!5m2!1spt-BR!2sbr" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                     
+                  </div>
+
+                  <div class="pgContato__local">
+                     <h2 class="pgContato__local-title">Clube de Campo</h2>
+                     <address>
+                        <div class="pgContato__local-item">
+                           <i class="fa-solid fa-location-dot pgContato__local-ico"></i>
+                           <p class="pgContato__local-endereco"> Estr. Mun. Nelson Tavares da Silva, 3001 - Bom Retiro, São José dos Campos - SP, 12226-205</p>
+                        </div>                        
+                        <div class="pgContato__local-item">
+                        <i class="fa-brands fa-square-whatsapp pgContato__local-ico"></i>
+                           <p class="pgContato__local-tel">(12) 3922-7424</p>
+                        </div>                        
+                     </address>     
+                  </div>
+
+               </div>  
+
+               
+               <div class="pgContato__item">
+                  <div class="pgContato__mapa">
+
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d468753.2109835335!2d-45.8847493!3d-23.3833525!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cd60d482afc101%3A0x2ff90a4d7f00570b!2sPousada%20ASSEM%20Litoral!5e0!3m2!1spt-BR!2sbr!4v1679598474755!5m2!1spt-BR!2sbr" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                     
+                  </div>
+
+                  <div class="pgContato__local">
+                     <h2 class="pgContato__local-title">Pousada Litoral</h2>
+                     <address>
+                        <div class="pgContato__local-item">
+                           <i class="fa-solid fa-location-dot pgContato__local-ico"></i>
+                           <p class="pgContato__local-endereco"> Av. Regina Margarete Passos, 27 - Massaguaçu, Caraguatatuba - SP, 11677-340</p>
+                        </div>                        
+                        <div class="pgContato__local-item">
+                        <i class="fa-brands fa-square-whatsapp pgContato__local-ico"></i>
+                           <p class="pgContato__local-tel">(12) 3884-2910</p>
+                        </div>                        
+                     </address>     
+                  </div>
+
+               </div>  
+
+            </div>        
+         
+         </div>
+
+      </section>
+
+
+   <?php endwhile; else: ?>
+   <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+   <?php endif; ?>
+
+<?php get_footer(); ?>
