f31a7e5094204ab56d7688e3fe1b1390eecf3f85
Danilo Augusto <daniloaugusto9101@gmail.com>
2023-03-30 12:04:05 -0300

    Update page historia

--- page-historia.php
+++ page-historia.php
@@ -6,9 +6,10 @@
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
     <section class="pgHistoria">
-        <div class="container pgHistoria__container">
+
+        <div class="container">
             <h1 class="tituloHr pgHistoria__tituloHr">Nossa História</h1>
-            <div class="pgHistoria__resumo">
+            <div class="pgHistoria__header">
                 <p>
                     Era março de 1979, dia 23, quando um grupo de servidores idealizou um lugar que promovesse a integração
                     dos servidores, onde pudessem se beneficiar de produtos e serviços, como lazer, saúde e descontração.
@@ -19,154 +20,220 @@
                     De lá para cá, muitos desafios, mas também, muitas conquistas.
                 </p>
             </div>
+
             <div class="pgHistoria__content">
 
+                <!-- Inicio Item -->
                 <div class="pgHistoria__item">
-                    <div class="pgHistoria__item-col">
-                        <div class="pgHistoria__item-container">
+                    <div class="pgHistoria__item-col">       
+                        <div>
                             <h2 class="pgHistoria__item-titulo">1979</h2>
-                            <p class="pgHistoria__item-texto">Nasce a ASSEM. A Sede Social passa a operar numa sala
-                                comercial, no Vip Center, região central de São José.</p>
+                            <p class="pgHistoria__item-texto">
+                                Nasce a ASSEM. A Sede Social passa a operar numa sala comercial, no Vip Center, região central de São José.
+                            </p>
                         </div>
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/page-historia/historia_1979.jpg" alt="">
+                        <div> 
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/page-historia/historia_1979.webp"  class="pgHistoria__item-img">
+                        </div>
                     </div>
                     <div class="pgHistoria__item-col">
-                        <!-- teste -->
+                           
                     </div>
                 </div>
+                <!-- Fim item -->
+
+                <!-- Inicio Item -->
                 <div class="pgHistoria__item">
-                    <div class="pgHistoria__item-col">
-                        <div class="pgHistoria__item-container">
+                    <div class="pgHistoria__item-col">       
+                        <div>
                             <h2 class="pgHistoria__item-titulo">1980</h2>
-                            <p class="pgHistoria__item-texto">Durante os próximos 14 anos, a Sede Social da ASSEM
-                                funcionou em sala improvisada, no piso térreo do Paço.</p>
+                            <p class="pgHistoria__item-texto">
+                            Durante os próximos 14 anos, a Sede Social da ASSEM funcionou em sala improvisada, no piso térreo do Paço.
+                            </p>
                         </div>
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/page-historia/historia_1980.jpg" alt="">
+                        <div> 
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/page-historia/historia_1980.webp" class="pgHistoria__item-img">
+                        </div>
                     </div>
                     <div class="pgHistoria__item-col">
-                        <!-- teste -->
+                           
                     </div>
                 </div>
+                <!-- Fim item -->
+
+                <!-- Inicio Item -->
                 <div class="pgHistoria__item">
-                    <div class="pgHistoria__item-col">
-                        <div class="pgHistoria__item-container">
+                    <div class="pgHistoria__item-col">       
+                        <div>
                             <h2 class="pgHistoria__item-titulo">1980/1985</h2>
-                            <p class="pgHistoria__item-texto">Começa a funcionar o primeiro clube de campo (junto a área
-                                do seminário na Av. São João).</p>
+                            <p class="pgHistoria__item-texto">
+                            Começa a funcionar o primeiro clube de campo (junto a área do seminário na Av. São João).
+                            </p>
                         </div>
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/page-historia/historia_1985.jpg" alt="">
+                        <div> 
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/page-historia/historia_1985.webp" class="pgHistoria__item-img">
+                        </div>
                     </div>
                     <div class="pgHistoria__item-col">
-                        <!-- teste -->
+                           
                     </div>
                 </div>
+                <!-- Fim item -->
+
+                <!-- Inicio Item -->
                 <div class="pgHistoria__item">
-                    <div class="pgHistoria__item-col">
-                        <div class="pgHistoria__item-container">
+                    <div class="pgHistoria__item-col">       
+                        <div>
                             <h2 class="pgHistoria__item-titulo">1986</h2>
-                            <p class="pgHistoria__item-texto">A Prefeitura adquire o Clube de Campo no Bom Retiro e cede
-                                para a ASSEM.</p>
+                            <p class="pgHistoria__item-texto">
+                            A Prefeitura adquire o Clube de Campo no Bom Retiro e cede para a ASSEM.
+                            </p>
                         </div>
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/page-historia/historia_1986.jpg" alt="">
+                        <div> 
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/page-historia/historia_1986.webp"  class="pgHistoria__item-img">
+                        </div>
                     </div>
                     <div class="pgHistoria__item-col">
-                        <!-- teste -->
+                           
                     </div>
                 </div>
+                <!-- Fim item -->
+
+                <!-- Inicio Item -->
                 <div class="pgHistoria__item">
-                    <div class="pgHistoria__item-col">
-                        <div class="pgHistoria__item-container">
+                    <div class="pgHistoria__item-col">       
+                        <div>
                             <h2 class="pgHistoria__item-titulo">1994</h2>
-                            <p class="pgHistoria__item-texto">A Sede muda de endereço e vai para um prédio em frente ao
-                                Paço Municipal.</p>
+                            <p class="pgHistoria__item-texto">
+                            A Sede muda de endereço e vai para um prédio em frente ao Paço Municipal.
+                            </p>
                         </div>
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/page-historia/historia_1994.jpg" alt="">
+                        <div> 
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/page-historia/historia_1994.webp"  class="pgHistoria__item-img">
+                        </div>
                     </div>
                     <div class="pgHistoria__item-col">
-                        <!-- teste -->
+                           
                     </div>
                 </div>
+                <!-- Fim item -->
+
+                <!-- Inicio Item -->
                 <div class="pgHistoria__item">
-                    <div class="pgHistoria__item-col">
-                        <div class="pgHistoria__item-container">
+                    <div class="pgHistoria__item-col">       
+                        <div>
                             <h2 class="pgHistoria__item-titulo">1995</h2>
-                            <p class="pgHistoria__item-texto">Sede própria é comprada na Rua Conselheiro Rodrigues
-                                Alves, nº 190, na Vila Santa Luzia.</p>
+                            <p class="pgHistoria__item-texto">
+                            Sede própria é comprada na Rua Conselheiro Rodrigues Alves, nº 190, na Vila Santa Luzia
+                            </p>
                         </div>
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/page-historia/historia_1995.jpg" alt="">
+                        <div> 
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/page-historia/historia_1995.webp"  class="pgHistoria__item-img">
+                        </div>
                     </div>
                     <div class="pgHistoria__item-col">
-                        <!-- teste -->
+                           
                     </div>
                 </div>
+                <!-- Fim item -->
+
+                <!-- Inicio Item -->
                 <div class="pgHistoria__item">
-                    <div class="pgHistoria__item-col">
-                        <div class="pgHistoria__item-container">
+                    <div class="pgHistoria__item-col">       
+                        <div>
                             <h2 class="pgHistoria__item-titulo">1996</h2>
-                            <p class="pgHistoria__item-texto">Nova Sede Social é inaugurada, e dá início a projetos de
-                                expansão em seus setores.</p>
+                            <p class="pgHistoria__item-texto">
+                            Nova Sede Social é inaugurada, e dá início a projetos de expansão em seus setores.
+                            </p>
                         </div>
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/page-historia/historia_1996.jpg" alt="">
+                        <div> 
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/page-historia/historia_1996.webp"  class="pgHistoria__item-img">
+                        </div>
                     </div>
                     <div class="pgHistoria__item-col">
-                        <!-- teste -->
+                           
                     </div>
                 </div>
+                <!-- Fim item -->
+
+                <!-- Inicio Item -->
                 <div class="pgHistoria__item">
-                    <div class="pgHistoria__item-col">
-                        <div class="pgHistoria__item-container">
+                    <div class="pgHistoria__item-col">       
+                        <div>
                             <h2 class="pgHistoria__item-titulo">2006</h2>
-                            <p class="pgHistoria__item-texto">ASSEM adquire uma Pousada em Caraguatatuba, no litoral
-                                norte de SP.</p>
+                            <p class="pgHistoria__item-texto">
+                            ASSEM adquire uma Pousada em Caraguatatuba, no litoral norte de SP
+                            </p>
                         </div>
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/page-historia/historia_2006.jpg" alt="">
+                        <div> 
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/page-historia/historia_2006.webp"  class="pgHistoria__item-img">
+                        </div>
                     </div>
                     <div class="pgHistoria__item-col">
-                        <!-- teste -->
+                           
                     </div>
                 </div>
+                <!-- Fim item -->
+
+                <!-- Inicio Item -->
                 <div class="pgHistoria__item">
-                    <div class="pgHistoria__item-col">
-                        <div class="pgHistoria__item-container">
+                    <div class="pgHistoria__item-col">       
+                        <div>
                             <h2 class="pgHistoria__item-titulo">2013</h2>
-                            <p class="pgHistoria__item-texto">Início das reformas na Sede Social da ASSEM e ampliação de
-                                área.</p>
+                            <p class="pgHistoria__item-texto">
+                            Início das reformas na Sede Social da ASSEM e ampliação de área.
+                            </p>
                         </div>
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/page-historia/historia_2013.jpg" alt="">
+                        <div> 
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/page-historia/historia_2013.webp"  class="pgHistoria__item-img">
+                        </div>
                     </div>
                     <div class="pgHistoria__item-col">
-                        <!-- teste -->
+                           
                     </div>
                 </div>
+                <!-- Fim item -->
+
+                <!-- Inicio Item -->
                 <div class="pgHistoria__item">
-                    <div class="pgHistoria__item-col">
-                        <div class="pgHistoria__item-container">
+                    <div class="pgHistoria__item-col">       
+                        <div>
                             <h2 class="pgHistoria__item-titulo">2014</h2>
-                            <p class="pgHistoria__item-texto">Aquisição de bens e oferta de mais serviços aos
-                                associados, como a compra de TVs de 32” em LED para os quartos da Pousada ASSEM Litoral
-                                e reforma e ampliação na Sede Social são concluídas, resultando em uma academia com 3
-                                ambientes para os seus associados.</p>
+                            <p class="pgHistoria__item-texto">
+                            Aquisição de bens e oferta de mais serviços aos associados, como a compra de TVs de 32” em LED para os quartos da Pousada ASSEM Litoral e reforma e ampliação na Sede Social são concluídas, resultando em uma academia com 3 ambientes para os seus associados.
+                            </p>
                         </div>
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/page-historia/historia_2014.jpg" alt="">
+                        <div> 
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/page-historia/historia_2014.webp"  class="pgHistoria__item-img">
+                        </div>
                     </div>
                     <div class="pgHistoria__item-col">
-                        <!-- teste -->
+                           
                     </div>
                 </div>
+                <!-- Fim item -->
+
+                <!-- Inicio Item -->
                 <div class="pgHistoria__item">
-                    <div class="pgHistoria__item-col">
-                        <div class="pgHistoria__item-container">
+                    <div class="pgHistoria__item-col">       
+                        <div>
                             <h2 class="pgHistoria__item-titulo">2015</h2>
-                            <p class="pgHistoria__item-texto">Sede Social ganhou sistema de videomonitoramento e
-                                catracas para controle de acesso e mais segurança. O Clube de Campo ganhou toboágua e
-                                escorregador para mais diversão do associado nas piscinas do clube.</p>
+                            <p class="pgHistoria__item-texto">
+                            Sede Social ganhou sistema de videomonitoramento e catracas para controle de acesso e mais segurança. O Clube de Campo ganhou toboágua e escorregador para mais diversão do associado nas piscinas do clube.
+                            </p>
                         </div>
-                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/page-historia/historia_2015.jpg" alt="">
+                        <div> 
+                            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/page-historia/historia_2015.webp"  class="pgHistoria__item-img">
+                        </div>
                     </div>
                     <div class="pgHistoria__item-col">
-                        <!-- teste -->
+                           
                     </div>
                 </div>
+                <!-- Fim item -->
+
+                
+
 
             </div>
         </div>
